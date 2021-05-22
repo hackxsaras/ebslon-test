de825e95e0c1eddb3baaeadf259b291b1e8c583e
hackxsaras <62321819+hackxsaras@users.noreply.github.com>
2021-05-22 15:16:19 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,123 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Dora Great</title>
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+		<div class="header flex">
+			<img class="logo" src = "img/logo.png">
+			<div class="flexy"></div>
+			<div class="menu">
+				<div class="item">About</div>
+				<div class="item">Our Approach</div>
+				<div class="item">Blog</div>
+				<div class="item">Contact</div>
+			</div>
+			<div class="toggler" onclick="toggleMenu()"><div>V</div></div>
+		</div>
+		<div class="fc">
+			<div class="section landing-container">
+				<div class="center landing-text">
+					Discover a better version of <u>you</u>. <br />
+					<button>Schedule your intro call now</button>
+				</div>
+			</div>
+
+			<div class="section personal-approach">
+				<div class="h1 ctext">Our Personalized Approach</div>
+				<div class="flex">
+					<div class="card">
+						<img src="img/3.png">
+						<div class="text">
+							<h1>Weekly One-on-One Coaching and Virtual Support as Needed</h1>
+							Customized, private life coaching to discuss your big-picture ambitions and work with Kate to translate them into actionable steps.
+						</div>
+					</div>
+
+					<div class="card">
+						<img src="img/2.png">
+						<div class="text">
+							<h1>Unlimited Access to Exclusive Digital Content</h1>
+							As a private client, you’ll have complete access to monthly training videos, webinars, and downloadable tools made specifically for executive women.
+						</div>
+					</div>
+					<div class="card">
+						<img src="img/1.png">
+						<div class="text">
+							<h1>Membership to a Community of Professional Women</h1>
+							When we each succeed, we all succeed. This online group is a safe place to celebrate, confide in, and lean on one another. 
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="section customers-say">
+				<div class="h1">What Our Clients Say?</div>
+				<div class="review">
+					<div class="profile">
+						<div class="name">Jessica Ole says..</div>
+						<div class="img"><img src="img/stella.jpg"></div>
+					</div>
+					<div class="text">“Kate is one of the most incredible coaches you will find! Her energy is contagious and she really brings out the best in you. She helps you tap into your own wisdom and helps you unlock your personal power. I absolutely loved working with her!”</div>
+				</div>
+				<div class="review">
+					<div class="profile">
+						<div class="name">Stella - Oceanside, CA says..</div>
+						<div class="img"><img src="img/stella.jpg"></div>
+					</div>
+					<div class="text">“Before coaching with Kate, I struggled with low self-esteem, being unproductive, and not taking care of my mind and body. Then, thanks to Kate, I realized my worth. The most significant results I’ve experienced from coaching with Kate are feeling more confident than I ever have, starting my own small business, and having a healthy and happy body.”</div>
+				</div>
+				<div class="review">
+					<div class="profile">
+						<div class="name">Jessica Ole says..</div>
+						<div class="img"><img src="img/stella.jpg"></div>
+					</div>
+					<div class="text">“Kate easily develops rapport with clients and really makes them feel heard and understood. She connects with a positive and motivating energy, sharing and empathizing with client's goals, stories, and challenges. Kate is a wonderful coach and I would love to work with her again!”</div>
+				</div>
+				<div class="review">
+					<div class="profile">
+						<div class="name">Jessica Ole says..</div>
+						<div class="img"><img src="img/stella.jpg"></div>
+					</div>
+					<div class="text">“Kate is one of the most incredible coaches you will find! Her energy is contagious and she really brings out the best in you. She helps you tap into your own wisdom and helps you unlock your personal power. I absolutely loved working with her!”</div>
+				</div>
+				<div class="review">
+					<div class="profile">
+						<div class="name">Stella - Oceanside, CA says..</div>
+						<div class="img"><img src="img/stella.jpg"></div>
+					</div>
+					<div class="text">“Before coaching with Kate, I struggled with low self-esteem, being unproductive, and not taking care of my mind and body. Then, thanks to Kate, I realized my worth. The most significant results I’ve experienced from coaching with Kate are feeling more confident than I ever have, starting my own small business, and having a healthy and happy body.”</div>
+				</div>
+				<div class="review">
+					<div class="profile">
+						<div class="name">Jessica Ole says..</div>
+						<div class="img"><img src="img/stella.jpg"></div>
+					</div>
+					<div class="text">“Kate easily develops rapport with clients and really makes them feel heard and understood. She connects with a positive and motivating energy, sharing and empathizing with client's goals, stories, and challenges. Kate is a wonderful coach and I would love to work with her again!”</div>
+				</div>
+			</div>
+			<div class="section end flex">
+				<form>
+					<h1>Schedule a Call</h1>
+					Full Name:<input name="name" placeholder="Your name" required>
+					Email Address:<input type="email" placeholder="Your email address" required>
+					Phone No.:<input type="phone" placeholder="Your phone no." required>
+					Best Time to Call:<input type="time" value = "12:01" required>
+					<button type="submit" style="font-size:1em;">SUBMIT</button>
+				</form>
+				<div class="about flexy">
+					<!-- <div class="flex"> -->
+						<div>
+							<div class="h1" style="color:#fff">Shattered Glass Coaching</div>
+							<div class="h1" style="color:#fff; font-size:2em;">Personalized life coaching for the modern woman.
+						</div>
+						<img class="life" src = "img/life.png">
+						<div class="h1" style="color:#fff; font-size:1em;">Website designed by <u><a href="https://hackxsaras.github.io"> hackxsaras</a></u></div>
+					<!-- </div> -->
+				</div>
+			</div>
+		</div>
+
+		<script src="js/main.js"></script>
+	</body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,4 @@
+function toggleMenu(){
+	document.getElementsByClassName('menu')[0].classList.toggle('opened');
+	document.getElementsByClassName('toggler')[0].classList.toggle('opened');
+}
